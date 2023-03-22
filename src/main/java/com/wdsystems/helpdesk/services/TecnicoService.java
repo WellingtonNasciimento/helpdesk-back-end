66cfdbf9c0102956336628404fb53fe3e562d3d5
WellingtonNasciimento <wellington.d.nascimento@uol.com.br>
2023-03-22 16:43:28 -0300

    Exception for Object Not Found

--- src/main/java/com/wdsystems/helpdesk/services/TecnicoService.java
+++ src/main/java/com/wdsystems/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.wdsystems.helpdesk.domain.Tecnico;
 import com.wdsystems.helpdesk.repositories.TecnicoRepository;
+import com.wdsystems.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
@@ -14,9 +15,9 @@ public class TecnicoService {
 	@Autowired
 	private TecnicoRepository tecnicoRepository;
 	
-	public Tecnico findByID(Integer Id) {
+	public Tecnico findByID(Integer id) {
 		
-		Optional<Tecnico> obj = tecnicoRepository.findById(Id);
-		return obj.orElse(null);
+		Optional<Tecnico> obj = tecnicoRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Tecnico Not Found - ID: " + id));
 	}
 }
